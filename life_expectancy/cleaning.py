b495674281c00c43a31edabbb29ca9a999e29f05
lfmelo <luis.f.melo@nos.pt>
2023-05-04 15:38:02 +0100

    split clean data into 3 functions

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,22 +1,37 @@
 import argparse
+from dataclasses import dataclass
 import pandas as pd
 
-def clean_data(country = 'PT'):
+def load_data():
     '''
     Cleans the data
     '''
     # load input dataframe
-    life_expectancy = pd.read_csv('life_expectancy/data/eu_life_expectancy_raw.tsv', sep='\t')
+    data = pd.read_csv('life_expectancy/data/eu_life_expectancy_raw.tsv', sep='\t')
+
+    return data
+
+def save_data(data):
+    '''
+    Cleans the data
+    '''
+
+    data.to_csv('life_expectancy/data/pt_life_expectancy.csv', index = False)
+
+def clean_data(data, country = 'PT'):
+    '''
+    Cleans the data
+    '''
 
     ids = ['unit', 'sex', 'age', 'region']
     # split the first columns into 4 diferent columns using , as separator
-    life_expectancy[ids] = life_expectancy['unit,sex,age,geo\\time'].str.split(',', expand=True)
+    data[ids] = data['unit,sex,age,geo\\time'].str.split(',', expand=True)
 
     # drop unecessary column
-    life_expectancy = life_expectancy.drop(columns=['unit,sex,age,geo\\time'])
+    data = data.drop(columns=['unit,sex,age,geo\\time'])
 
     # unpivot the dataset to long format
-    melted_df = pd.melt(life_expectancy, value_vars = life_expectancy.drop(columns = ids).columns,
+    melted_df = pd.melt(data, value_vars = data.drop(columns = ids).columns,
           id_vars = ids, var_name = 'year', value_name='value')
 
     # ensures year is a int
@@ -26,13 +41,25 @@ def clean_data(country = 'PT'):
     melted_df['value'] = (melted_df['value'].str.extract(r'(\d+\.?\d*)').astype(float))
     cleaned_df = melted_df.dropna()
 
+    # filter for country
     filtered_df = cleaned_df[cleaned_df['region'] == country]
 
-    filtered_df.to_csv('life_expectancy/data/pt_life_expectancy.csv', index = False)
+    return filtered_df
+
+def main(country = 'PT'):
+    """
+    main function
+    """
+    data = load_data()
+    cleaned_data = clean_data(data, country)
+    save_data(cleaned_data)
 
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser(description='Clean data and filter by country')
     parser.add_argument('--country', help='Country to use as filter')
     args = parser.parse_args()
-    clean_data(args)
+    
+    main(args["country"])
+
+    
     
